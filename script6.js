4576d27fcedcdce0fdbf8031404d671faae83a00
sudaisamir <139509085+sudaisamir@users.noreply.github.com>
2023-10-21 03:11:51 +0500

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,29 @@
+// function userNumber(num1, num2, oprator){
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// if(isNaN(num1) || isNaN(num2)){
+//   return 'Invalid input. Please enter a valid number'
+// }
+// switch(oprator){
+//   case "+":
+//     return num1+num2;
+//     case "-":
+//       return num1-num2;
+//       case "*":
+//         return num1*num2;
+//         case "/":
+//           if(num2===0){
+//             return 'Division by zero is not allowed.';
+//           }
+//           return num1/num2;
+//           default:
+//             return 'Invalid operator. Please use +, -, *, or /.';
+// }
+// }
+
+// let num1 = prompt("Enter your First Number");
+// let num2 = prompt("Enter your First Number");
+// let oprator = prompt('Enter the operator (+, -, *, /):');
+// let result = userNumber(num1,num2,oprator)
+
+// document.write('Result: ' + result)
